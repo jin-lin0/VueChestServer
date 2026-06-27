1e988d12161cc5c91699f2b8a2276baaa30f65a8
hejinlin <769316680@qq.com>
2026-06-27 20:15:31 +0800

    fix: 上传app最大2mb

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 // 中间件
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "2mb" }));
 
 // 访问统计（记录所有 API 请求）
 const visitLogger = require("./middleware/visitLogger");
